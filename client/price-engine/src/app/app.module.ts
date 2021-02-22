8b43afa2bc1585d130f3932f97abf1f1bf34aff8
DESKTOP-M3T0SOU\tharu <tharushashehan@gmail.com>
2021-02-22 11:25:30 +0530

    Added login page and completed product page

--- client/price-engine/src/app/app.module.ts
+++ client/price-engine/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 
 import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'product', component: ProductComponent , data: { title: 'Product page' } },
+  { path: 'login', component: LoginComponent , data: { title: 'Login page' } }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    HttpClientModule
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    MatTableModule
+    MatTableModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes), 
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- client/price-engine/src/app/product/product.component.ts
+++ client/price-engine/src/app/product/product.component.ts
@@ -3,22 +3,28 @@ import { HttpClient } from '@angular/common/http';
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  id: number;
+  //url: String,
+  description: String,
+  status: String,
+  expiry_date: String,
+  //featured: String,
+  //image: String,
+  //user_id: number;
+  //created_at: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {id: 1, name: 'Hydrogen', description: 'Hydrogen', status: 'H', expiry_date: '0000-00-00 00:00:00'},
+  {id: 2, name: 'Helium', description: 'Hydrogen', status: 'He', expiry_date: '0000-00-00 00:00:00'},
+  {id: 3, name: 'Lithium', description: 'Hydrogen', status: 'Li', expiry_date: '0000-00-00 00:00:00'},
+  {id: 4, name: 'Beryllium', description: 'Hydrogen', status: 'Be', expiry_date: '0000-00-00 00:00:00'},
+  {id: 5, name: 'Boron', description: 'Hydrogen', status: 'B', expiry_date: '0000-00-00 00:00:00'},
+  {id: 6, name: 'Carbon', description: 'Hydrogen', status: 'C', expiry_date: '0000-00-00 00:00:00'},
+  {id: 7, name: 'Nitrogen', description: 'Hydrogen', status: 'N', expiry_date: '0000-00-00 00:00:00'},
+  {id: 8, name: 'Oxygen', description: 'Hydrogen', status: 'O', expiry_date: '0000-00-00 00:00:00'},
+  {id: 9, name: 'Fluorine', description: 'Hydrogen', status: 'F', expiry_date: '0000-00-00 00:00:00'},
+  {id: 10, name: 'Neon', description: 'Hydrogen', status: 'Ne', expiry_date: '0000-00-00 00:00:00'},
 ];
 
 
@@ -32,13 +38,23 @@ export class ProductComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() : void {
-    this.http.get<any>('http://127.0.0.1:8080/product/product-list').subscribe(
+
+    const headerDict = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT'
+    }
+    const requestOptions : Object = {                                                                                                                                                                                 
+      headers: new Headers(headerDict)
+    };
+    this.http.get<String>('http://demo.dreamsquadgroup.com/test/index.json', requestOptions).subscribe(
     data => console.log(data),
     error => console.error('There was an error!', error)
 )
   }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'description', 'status', 'expiry_date'];
   dataSource = ELEMENT_DATA;
 
 }
